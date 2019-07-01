c9bfcba2b7c1e329407d48ca67e35cd5ab065971
Peter <peter@engineeringscotland.co.uk>
2019-07-01 21:37:31 +0100

    fixed a few bits
    added a demo file

--- prepper.py
+++ prepper.py
@@ -6,10 +6,18 @@ import matplotlib.pyplot as plt
 
 from math import log2, pow
 
+settingsDict={
+'startThreash': '0.01',
+'endThreash': '0.005',
+'zeroLength': '0.01',
+'minSampleLength': '0.2',
+'preDelay': '0.1',
+'postDelay': '0.25'}
+
 A4 = 440
 C0 = A4*pow(2, -4.75)
 name = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-    
+
 def pitch(freq):
     p = (12*log2(freq/C0))
     h = round(p)
@@ -19,7 +27,6 @@ def pitch(freq):
     octave = h // 12
     n = h % 12
     return name[n] + str(octave) + ' + ' + str(rem) + ' cents'
-
 
 if '__file__' in locals():
     #print('running from file')
@@ -37,13 +44,10 @@ files = os.listdir(os.getcwd())
 if not os.path.isfile('settings.txt'):
    
     f = open('settings.txt','w+')
-    f.write('''zeroThreash=0.01
-zeroLength=0.01
-minSampleLength=0.2
-preDelay=0.1
-postDelay=1''')
-    
+    for setting in settingsDict:
+        f.write(setting + '=' + settingsDict[setting] +'\n')
     f.close()
+    
 else:
     f = open('settings.txt')
     settings = f.readlines()
@@ -70,8 +74,6 @@ else:
             fs, data = wave.read(file)
             zeroLengthSamples = round(float(settingsDict['zeroLength']) * fs)
             
-
-            
             preDelaySamples = int(float(settingsDict['preDelay']) * fs)
             postDelaySamples = int(float(settingsDict['postDelay']) * fs)
             minSampleLengthSamples = int(float(settingsDict['minSampleLength']) * fs)
@@ -85,8 +87,8 @@ else:
             monoEnvelopeSmooth = monoEnvelopeSmooth.astype('int')
             
             waveMax = max( np.iinfo(left.dtype).max, -np.iinfo(left.dtype).min)
-            zeroThreashWindow = round(float(settingsDict['zeroThreash']) * waveMax)
-            
+            startThreashWindow = round(float(settingsDict['startThreash']) * waveMax)
+            endThreashWindow = round(float(settingsDict['endThreash']) * waveMax)
             
             clips = [] 
             currentclip = [0, 0]
@@ -94,26 +96,26 @@ else:
             inclip = False
             for i in range(len(monoEnvelopeSmooth)):
                 point = int(monoEnvelopeSmooth[i])
-                if not inclip and point > zeroThreashWindow:
+                if not inclip and point > startThreashWindow:
                      inclip = True
                      currentclip[0] = max (lastend, i - preDelaySamples)
-                if inclip and point < zeroThreashWindow  :
+                if inclip and point < endThreashWindow  :
                      inclip = False
-                     lastend = min (i + preDelaySamples, len(monoEnvelopeSmooth) - 1)
+                     lastend = min (i + postDelaySamples, len(monoEnvelopeSmooth) - 1)
                      currentclip[1] = lastend
                      length = currentclip[1] - currentclip[0]
                      if length > minSampleLengthSamples:
                         clips.append(currentclip) 
                         currentclip = [0, 0]                        
             print (clips)
-                     
             
             plt.figure(figsize=(10,10))
             plt.plot(monoRectified)
             plt.plot(monoEnvelopeSmooth)
-            plt.plot([0, monoRectified.size], [zeroThreashWindow, zeroThreashWindow], c = 'red')
-            plt.scatter(np.array(clips)[:,0], zeroThreashWindow * np.ones(len(clips)), c = 'black', s = 2)
-            plt.scatter(np.array(clips)[:,1], zeroThreashWindow * np.ones(len(clips)), c = 'grey', s = 2)
+            plt.plot([0, monoRectified.size], [startThreashWindow, startThreashWindow], c = 'red')
+            plt.plot([0, monoRectified.size], [endThreashWindow, endThreashWindow], c = 'green')
+            plt.scatter(np.array(clips)[:,0], startThreashWindow * np.ones(len(clips)), c = 'black', s = 6, zorder = 5)
+            plt.scatter(np.array(clips)[:,1], endThreashWindow * np.ones(len(clips)), c = 'grey', s = 6, zorder = 5)
             plt.savefig(file +'.jpg', dpi = 500)
             
             clipno = 1
@@ -130,5 +132,3 @@ else:
                     clipno += 1
                     wave.write(clipFilename, fs, clipData)
             os.chdir('..')
-            
-            
